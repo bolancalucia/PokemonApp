f2f9713b460150c84ae60569743e76d8628bb2cd
Lucia Bolanča <bolancalucia@gmail.com>
2018-04-03 11:12:07 +0200

    Changes with JSON Objects

--- app/src/main/java/udacity/pokemon/MainActivity.java
+++ app/src/main/java/udacity/pokemon/MainActivity.java
@@ -59,11 +59,9 @@ public class MainActivity extends AppCompatActivity {
             HttpHandler sh = new HttpHandler();
 
             String url = "https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json";
-
-
             String jsonString = "";
             try {
-                // TODO: make a request to the URL
+                jsonString = sh.makeHttpRequest(createUrl(url));
 
             } catch (IOException e) {
                 return null;
@@ -72,15 +70,14 @@ public class MainActivity extends AppCompatActivity {
             Log.e(TAG, "Response from url: " + jsonString);
             if (jsonString != null) {
                 try {
-                    //TODO: Create a new JSONObject
-
-                    // TODO: Get the JSON Array node and name it "pokemons"
-
-
+                    JSONObject jsonObj = new JSONObject(jsonString);
+                    JSONArray pokemons = jsonObj.getJSONArray("pokemon");
                     // looping through all Contacts
                     for (int i = 0; i < pokemons.length(); i++) {
-                        //TODO: get the JSONObject and its three attributes
-
+                        JSONObject currentPokemon = pokemons.getJSONObject(i);
+                        String name = currentPokemon.getString("name");
+                        String id = currentPokemon.getString("id");
+                        String candy = currentPokemon.getString("candy");
 
                         // tmp hash map for a single pokemon
                         HashMap<String, String> pokemon = new HashMap<>();
